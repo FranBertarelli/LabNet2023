0150b940d327145409ab6ce480cacc4da3836dfe
FranBertarelli <franbertarelli@gmail.com>
2023-08-18 18:27:11 -0300

    Dejo terminado el TBPOO1 (creo)

---
+++ labNetPractica1/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labNetPractica1
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Simulador de Transportes";
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("¡            Simulador de        ");
+            Console.WriteLine("             Transportes                !");
+            Console.WriteLine("------------------------------------------");
+
+            Console.ResetColor();
+            Console.WriteLine();
+
+            List<TransportePublico> transportes = GenerarTransportes();
+
+            MostrarTransportes(transportes);
+
+            Console.WriteLine("\nPresiona Enter para salir...gracias por usarme");
+            Console.ReadLine();
+        }
+
+        static List<TransportePublico> GenerarTransportes()
+        {
+            List<TransportePublico> transportes = new List<TransportePublico>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Omnibus omnibus = new Omnibus();
+                int pasajeros;
+                while (true)
+                {
+                    Console.Write($"Ingresa el número de pasajeros para el ómnibus {omnibus.Numero} (máximo 100): ");
+                    if (int.TryParse(Console.ReadLine(), out pasajeros) && pasajeros >= 0 && pasajeros <= 100)
+                    {
+                        omnibus.Pasajeros = pasajeros;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("ingresa un número entero positivo entre 0 y 100. dale...");
+                    }
+                }
+                transportes.Add(omnibus);
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Taxi taxi = new Taxi();
+                int pasajeros;
+                while (true)
+                {
+                    Console.Write($"Ingresa el número de pasajeros para el taxi {taxi.Numero} (máximo 4): ");
+                    if (int.TryParse(Console.ReadLine(), out pasajeros) && pasajeros >= 0 && pasajeros <= 4)
+                    {
+                        taxi.Pasajeros = pasajeros;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("ingresa un número entero positivo entre 0 y 4. Entende");
+                    }
+                }
+                transportes.Add(taxi);
+            }
+
+            return transportes;
+        }
+
+        static void MostrarTransportes(List<TransportePublico> transportes)
+        {
+            foreach (var transporte in transportes)
+            {
+                string tipo = (transporte is Omnibus) ? "Omnibus" : "Taxi";
+
+                if (tipo == "Omnibus")
+                {
+                    Omnibus omnibus = (Omnibus)transporte;
+                    Console.WriteLine($"\n{tipo} {omnibus.Numero}: {transporte.Pasajeros} pasajeros");
+                }
+                else if (tipo == "Taxi")
+                {
+                    Taxi taxi = (Taxi)transporte;
+                    Console.WriteLine($"\n{tipo} {taxi.Numero}: {transporte.Pasajeros} pasajeros");
+                }
+
+                transporte.Avanzar();
+                transporte.Detenerse();
+            }
+        }
+    }
+}
---
+++ labNetPractica1/TransportePublico.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labNetPractica1
+{
+    using System;
+
+    public abstract class TransportePublico
+    {
+        private int pasajeros;
+
+        public int Pasajeros
+        {
+            get { return pasajeros; }
+            set { pasajeros = value; }
+        }
+
+        public abstract void Avanzar();
+        public abstract void Detenerse();
+    }
+
+    public class Omnibus : TransportePublico
+    {
+        private bool avanzando = false;
+        private static int contador = 1;
+        private int numero;
+
+        public int Numero
+        {
+            get { return numero; }
+        }
+
+        public Omnibus()
+        {
+            numero = contador++;
+        }
+
+        public override void Avanzar()
+        {
+            if (!avanzando)
+            {
+                Console.WriteLine("Presiona 'Enter' para avanzar el ómnibus.");
+                Console.ReadLine();
+                Console.WriteLine($"El ómnibus {numero} está avanzando.");
+                avanzando = true;
+            }
+            else
+            {
+                Console.WriteLine($"\nEl ómnibus {numero} ya está avanzando.");
+            }
+        }
+
+        public override void Detenerse()
+        {
+            if (avanzando)
+            {
+                Console.WriteLine("Presiona 'enter' para detener el ómnibus.");
+                Console.ReadLine();
+                Console.WriteLine($"El ómnibus {numero} se ha detenido.");
+                avanzando = false;
+            }
+            else
+            {
+                Console.WriteLine($"\nEl ómnibus {numero} ya está detenido.");
+            }
+        }
+    }
+
+    public class Taxi : TransportePublico
+    {
+        private bool avanzando = false;
+        private static int contador = 1;
+        private int numero;
+
+        public int Numero
+        {
+            get { return numero; }
+        }
+
+        public Taxi()
+        {
+            numero = contador++;
+        }
+
+        public override void Avanzar()
+        {
+            if (!avanzando)
+            {
+                Console.WriteLine("Presiona 'Enter' para avanzar el taxi.");
+                Console.ReadLine();
+                Console.WriteLine($"El taxi {numero} está avanzando.");
+                avanzando = true;
+            }
+            else
+            {
+                Console.WriteLine($"\nEl taxi {numero} ya está avanzando.");
+            }
+        }
+
+        public override void Detenerse()
+        {
+            if (avanzando)
+            {
+                Console.WriteLine("Presione 'Enter' para detener el taxi.");
+                Console.ReadLine();
+                Console.WriteLine($"El taxi {numero} se ha detenido.");
+                avanzando = false;
+            }
+            else
+            {
+                Console.WriteLine($"\nEl taxi {numero} ya está detenido.");
+            }
+        }
+    }    
+}
