2429cdf4ee07e904396d297ca4bc94eac5451c60
FranBertarelli <franbertarelli@gmail.com>
2023-08-21 23:04:53 -0300

    Practica 2 punt 1 2 3 y 4

--- LabNetPractica2_2/LabNetPractica2_2/Program.cs
+++ LabNetPractica2_2/LabNetPractica2_2/Program.cs
@@ -4,12 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LabNetPractica2_2
+
+namespace DivisionExceptionExample
 {
-    internal class Program
+    public class DivisionPorCeroException : Exception
     {
-        static void Main(string[] args)
+        public DivisionPorCeroException() : base("AH NOOO, QUIEN SOS OPPENJEIMER O COMO SE ESCRIBA????" +
+            "")
+        { }
+    }
+
+    public class Calculator
+    {
+        public static double RealizarDivision(double dividendo, double divisor)
         {
+            if (divisor == 0)
+            {
+                throw new DivisionPorCeroException();
+            }
+
+            return dividendo / divisor;
+        }
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Ingresa el dividendo: ");
+                double dividendo = double.Parse(Console.ReadLine());
+
+                Console.Write("Ingresa el divisor: ");
+                double divisor = double.Parse(Console.ReadLine());
+
+                double resultado = RealizarDivision(dividendo, divisor);
+                Console.WriteLine($"La división {dividendo} / {divisor} es igual a {resultado}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(" pusiste una letra o no ingresaste nada GENIOUS");
+            }
+            catch (DivisionPorCeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Presiona cualquier tecla para salir del test");
+                Console.ReadKey();
+            }
         }
     }
 }
---
+++ LabNetPractica2_2/LabNetPractica2_2Tests/DivisionPorCeroExceptionTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DivisionExceptionExample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace DivisionExceptionExample.Tests
+{
+    [TestClass]
+    public class DivisionTests
+    {
+        [TestMethod]
+        public void TestDivisionValida()
+        {
+            // Arrange
+            double dividendo = 10;
+            double divisor = 2;
+
+            // Act
+            double resultado = Calculator.RealizarDivision(dividendo, divisor);
+
+            // Assert
+            Assert.AreEqual(5, resultado);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivisionPorCeroException))]
+        public void TestDivisionPorCero()
+        {
+            // Arrange
+            double dividendo = 8;
+            double divisor = 0;
+
+            // Act
+            Calculator.RealizarDivision(dividendo, divisor);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void TestEntradaInvalida()
+        {
+            // Arrange
+            double dividendo = 10;
+            string entradaDivisor = "abc";
+
+            // Act
+            double divisor = double.Parse(entradaDivisor);
+            Calculator.RealizarDivision(dividendo, divisor);
+        }
+    }
+}
